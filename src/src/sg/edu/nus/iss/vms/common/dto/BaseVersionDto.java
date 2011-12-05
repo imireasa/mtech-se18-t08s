a0585dce389bf134fbd42f447a18c059c05042df
a0065963 <a0065963@gmail.com>
2011-12-05 14:49:45 +0000

    security Dto

---
+++ src/src/sg/edu/nus/iss/vms/common/dto/BaseVersionDto.java
@@ -0,0 +1,61 @@
+package sg.edu.nus.iss.vms.common.dto;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class BaseVersionDto {
+	@Column(name = "version")
+	private Integer version;
+	@Column(name = "created_by")
+	private String createdBy;
+	@Column(name = "created_ts")
+	private Date createdTimeStamp;
+	@Column(name = "ls_upd_by")
+	private String lastUpdatedBy;
+	@Column(name = "ls_upd_ts")
+	private Date lastUpdatedTimeStamp;
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedTimeStamp() {
+		return createdTimeStamp;
+	}
+
+	public void setCreatedTimeStamp(Date createdTimeStamp) {
+		this.createdTimeStamp = createdTimeStamp;
+	}
+
+	public String getLastUpdatedBy() {
+		return lastUpdatedBy;
+	}
+
+	public void setLastUpdatedBy(String lastUpdatedBy) {
+		this.lastUpdatedBy = lastUpdatedBy;
+	}
+
+	public Date getLastUpdatedTimeStamp() {
+		return lastUpdatedTimeStamp;
+	}
+
+	public void setLastUpdatedTimeStamp(Date lastUpdatedTimeStamp) {
+		this.lastUpdatedTimeStamp = lastUpdatedTimeStamp;
+	}
+
+}
