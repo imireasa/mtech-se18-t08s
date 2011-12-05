84b97225f5dfdc7c0a415ca00f24eb6686517823
a0065963 <a0065963@gmail.com>
2011-12-05 15:33:33 +0000

    Login method

---
+++ src/src/sg/edu/nus/iss/vms/common/constants/VMSConstants.java
@@ -0,0 +1,7 @@
+package sg.edu.nus.iss.vms.common.constants;
+
+public class VMSConstants {
+
+	public static final String ACTIVE = "A";
+
+}
--- src/src/sg/edu/nus/iss/vms/security/service/impl/SecurityManagementServiceImpl.java
+++ src/src/sg/edu/nus/iss/vms/security/service/impl/SecurityManagementServiceImpl.java
@@ -1,16 +1,38 @@
 package sg.edu.nus.iss.vms.security.service.impl;
 
-import sg.edu.nus.iss.vms.common.SessionBean;
+import java.util.List;
+
+import sg.edu.nus.iss.vms.common.constants.VMSConstants;
+import sg.edu.nus.iss.vms.common.dao.Dao;
+import sg.edu.nus.iss.vms.common.exception.ApplicationException;
+import sg.edu.nus.iss.vms.security.dto.UserDto;
 import sg.edu.nus.iss.vms.security.service.SecurityManagementService;
 
 public class SecurityManagementServiceImpl implements SecurityManagementService{
-	private SessionBean sessionBean;
+	private static final String NAMED_QUERY_LOGIN = "login";
+	private Dao securityManagementDao;
 
-	public void setSessionBean(SessionBean sessionBean) {
-		this.sessionBean = sessionBean;
-	}
-
-	public SessionBean getSessionBean() {
-		return sessionBean;
+	public UserDto login(String username, String password) throws ApplicationException{
+		if((username == null || username.trim().equalsIgnoreCase("")) || 
+				(password==null || password.trim().equalsIgnoreCase(""))){
+			//username and password is not valid
+			return null;
+		}
+		String loginQuery=NAMED_QUERY_LOGIN;
+		String[] loginValues = new String[3];
+		loginValues[0] = username;
+		loginValues[1] = password;
+		loginValues[2] = VMSConstants.ACTIVE; //XXX active indicator maybe not in use.
+		List result = securityManagementDao.getObjectsByNamedQuery(loginQuery, loginValues, null);
+		if (result == null){
+			//TODO: username and /or password not valid. now return null.
+			return null;
+		}else if (result.size()>1){
+			//more than 1 result returned. wrong data for this user.
+			throw new ApplicationException("Corrupted data for this user. User ID is "+username);
+		}else{
+			//TODO: Login Audits will come here. if necessary
+			return (UserDto) result.get(0); //return the first object.
+		}
 	}
 }
