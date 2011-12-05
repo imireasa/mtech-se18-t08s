5c2c2264db7ddc1bb3679514219f5e0c7a96c801
a0065963@gmail.com <a0065963@gmail.com@737b461a-4eb2-ba18-f755-00632674a257>
2011-12-05 15:34:00 +0000

    Login method

--- trunk/src/src/sg/edu/nus/iss/vms/security/service/SecurityManagementService.java
+++ trunk/src/src/sg/edu/nus/iss/vms/security/service/SecurityManagementService.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss.vms.security.service;
 
-public interface SecurityManagementService {
+import sg.edu.nus.iss.vms.common.exception.ApplicationException;
+import sg.edu.nus.iss.vms.security.dto.UserDto;
 
+public interface SecurityManagementService {
+	public UserDto login(String username, String password) throws ApplicationException;
 }
