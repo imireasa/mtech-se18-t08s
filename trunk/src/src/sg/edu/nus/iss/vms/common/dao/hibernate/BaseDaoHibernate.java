0010e750f6c5a682af367a0f273baf69d881fb63
a0065963@gmail.com <a0065963@gmail.com@737b461a-4eb2-ba18-f755-00632674a257>
2011-12-25 12:00:06 +0000

    parameters to start from 1

--- trunk/src/src/sg/edu/nus/iss/vms/common/dao/hibernate/BaseDaoHibernate.java
+++ trunk/src/src/sg/edu/nus/iss/vms/common/dao/hibernate/BaseDaoHibernate.java
@@ -193,7 +193,7 @@ public class BaseDaoHibernate extends HibernateDaoSupport implements Dao {
 	public List getObjectsByNamedQuery(String namedQueryReference,  Object[] values,  QueryProperties properties) {
 		Query query = getSession().getNamedQuery(namedQueryReference);
 		if (values != null) {
-			for (int i = 0; i < values.length; i++) {
+			for (int i = 1; i <= values.length; i++) {
 				query.setParameter(i, values[i]); // assumes that HQL/SQL places in parameters using numbers.
 			}
 		}
