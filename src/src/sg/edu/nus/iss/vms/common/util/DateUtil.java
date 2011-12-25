fed86a36e76d5c08f8887e00c088b24c487acf5c
a0065963 <a0065963@gmail.com>
2011-12-25 15:24:55 +0000

    Clean up the import statement

--- src/src/sg/edu/nus/iss/vms/common/util/DateUtil.java
+++ src/src/sg/edu/nus/iss/vms/common/util/DateUtil.java
@@ -1,7 +1,6 @@
 package sg.edu.nus.iss.vms.common.util;
 
 import java.sql.Timestamp;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
