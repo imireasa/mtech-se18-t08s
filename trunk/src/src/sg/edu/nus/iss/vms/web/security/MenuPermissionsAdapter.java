1585954630dbdf1ba776b9321a2853a4fa08815d
a0065963@gmail.com <a0065963@gmail.com@737b461a-4eb2-ba18-f755-00632674a257>
2011-12-28 15:26:17 +0000

    log statement in adapter

--- trunk/src/src/sg/edu/nus/iss/vms/web/security/MenuPermissionsAdapter.java
+++ trunk/src/src/sg/edu/nus/iss/vms/web/security/MenuPermissionsAdapter.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.iss.vms.web.security;
 
+import org.apache.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,10 @@ import net.sf.navigator.menu.MenuComponent;
 import net.sf.navigator.menu.PermissionsAdapter;
 
 public class MenuPermissionsAdapter implements PermissionsAdapter{
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(MenuPermissionsAdapter.class);
 
 	private ArrayList<String> allowedMenus;
 	
@@ -21,7 +27,17 @@ public class MenuPermissionsAdapter implements PermissionsAdapter{
 
 	@Override
 	public boolean isAllowed(MenuComponent menu) {
-		return !allowedMenus.contains(menu.getName());
+		if (logger.isDebugEnabled()) {
+			logger.debug("isAllowed(MenuComponent) - start"); //$NON-NLS-1$
+		}
+		
+
+		boolean returnboolean = !allowedMenus.contains(menu.getName());
+		logger.debug("checking for "+menu.getName()+ "result is "+returnboolean);
+		if (logger.isDebugEnabled()) {
+			logger.debug("isAllowed(MenuComponent) - end"); //$NON-NLS-1$
+		}
+		return returnboolean;
 	}
 	
 
