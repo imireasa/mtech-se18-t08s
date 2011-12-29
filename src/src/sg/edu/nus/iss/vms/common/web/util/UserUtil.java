11018a6d668f1122156704df1ab086d63aa99d30
a0065963 <a0065963@gmail.com>
2011-12-29 11:46:15 +0000

    Get User session util

--- src/src/sg/edu/nus/iss/vms/common/web/util/UserUtil.java
+++ src/src/sg/edu/nus/iss/vms/common/web/util/UserUtil.java
@@ -1,9 +1,12 @@
 package sg.edu.nus.iss.vms.common.web.util;
 
-import org.apache.log4j.Logger;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import sg.edu.nus.iss.vms.common.Messages;
 import sg.edu.nus.iss.vms.common.vo.UserSessionInfoVo;
 
 /**
@@ -25,18 +28,11 @@ public class UserUtil {
      */
     public static UserSessionInfoVo getUserSessionInfoVo () {
         UserSessionInfoVo sessionInfo = null;
-        /*if(context == null){
-            return (UserSessionInfoVo) RequestContextHolder.getCaseConnectContext().getAttribute(Constants.USER_SESSION_INFO);
-        }
-        ExternalContext content = context.getExternalContext();
-        sessionInfo = (UserSessionInfoVo)content.getSessionMap().get(Constants.USER_SESSION_INFO);*/
-//        if(logger.isDebugEnabled()){
-//            logger.debug("######@@@@@@@@@Session Info@@@@@@@@@@@>>>>>>>>>>User Id:"+sessionInfo.getUserID());
-//            logger.debug("######@@@@@@@@@Session Info@@@@@@@@@@@>>>>>>>>>>User Name:"+sessionInfo.getName());
-//            logger.debug("######@@@@@@@@@Session Info@@@@@@@@@@@>>>>>>>>>>User Roles:"+sessionInfo.getRoles());
-//        }
         
-        //ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(session.getServletContext());
+        HttpServletRequest curRequest = 
+        	((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+        	.getRequest();
+        sessionInfo = (UserSessionInfoVo) curRequest.getSession().getAttribute((Messages.getString("AuthorisationFilter.SESSION_USER_ALLOWED_MENU_ATTR_NME")));
         return sessionInfo;
     }
 }
