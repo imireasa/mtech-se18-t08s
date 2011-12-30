cee0111438db2a311edd5d49f0fa9f5d9de0b8d6
zwhtet <zwhtet@737b461a-4eb2-ba18-f755-00632674a257>
2011-12-30 12:54:52 +0000

    Spring Mail Send and Receive Util for reset password function

--- trunk/src/src/sg/edu/nus/iss/vms/common/mail/BasicMailMessage.java
+++ trunk/src/src/sg/edu/nus/iss/vms/common/mail/BasicMailMessage.java
@@ -14,119 +14,113 @@ import org.springframework.mail.SimpleMailMessage;
  * @author zaw.htet
  *
  */
-public class BasicMailMessage extends SimpleMailMessage
-{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1385857811292272484L;
-	
-	protected String textTemplate;
-	protected Map textModel;
-	
-	protected Map attachments = new LinkedHashMap();
-	
-	public BasicMailMessage() {
-		super();
-	}
+public class BasicMailMessage extends SimpleMailMessage {
 
-	public BasicMailMessage(SimpleMailMessage original) {
-		super(original);
-	}
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 1385857811292272484L;
+        protected String textTemplate;
+        protected Map textModel;
+        protected Map attachments = new LinkedHashMap();
 
-	/**
-	 * @return the textTemplate
-	 */
-	public String getTextTemplate() {
-		return textTemplate;
-	}
+        public BasicMailMessage() {
+                super();
+        }
 
-    
-	/**
-	 * @param textTemplate the textTemplate to set
-	 */
-	public void setTextTemplate(String textTemplate) {
-		this.textTemplate = textTemplate;
-	}
+        public BasicMailMessage(SimpleMailMessage original) {
+                super(original);
+        }
 
-	/**
-	 * @return the textModel
-	 */
-	public Map getTextModel() {
-		return textModel;
-	}
+        /**
+         * @return the textTemplate
+         */
+        public String getTextTemplate() {
+                return textTemplate;
+        }
 
-	/**
-	 * @param textModel the textModel to set
-	 */
-	public void setTextModel(Map textModel) {
-		this.textModel = textModel;
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.springframework.mail.SimpleMailMessage#getText()
-	 */
-	@Override
-	public String getText() {
-		if (StringUtils.isNotBlank(textTemplate) && textModel != null && StringUtils.isBlank( super.getText() )) {
-			setText( StrSubstitutor.replace(textTemplate, textModel) );
-		}
-		
-		if (null != super.getText()) {
-			return super.getText();
-		}
-		
-		// force send empty text message
-		return "";
-	}
+        /**
+         * @param textTemplate the textTemplate to set
+         */
+        public void setTextTemplate(String textTemplate) {
+                this.textTemplate = textTemplate;
+        }
 
-	public boolean addFileAttachment(String fileAttachment)
-	{
-		File test = new File(fileAttachment);
-		boolean exist = test.exists();
-		
-		// add fileAttachment as key, and check is exist and save it as value of map
-		if (exist) {
-			attachments.put(fileAttachment, Boolean.valueOf(exist));
-		}
-		return exist;
-	}
+        /**
+         * @return the textModel
+         */
+        public Map getTextModel() {
+                return textModel;
+        }
 
-	public boolean setFileAttachment(String fileAttachment)
-	{
-		File test = new File(fileAttachment);
-		boolean exist = test.exists();
-		
-		// add fileAttachment as key, and check is exist and save it as value of map
-		if (exist) {
-			attachments.clear();
-			attachments.put(fileAttachment, Boolean.valueOf(exist));
-		}
-		return exist;
-	}
+        /**
+         * @param textModel the textModel to set
+         */
+        public void setTextModel(Map textModel) {
+                this.textModel = textModel;
+        }
 
-	public Object removeFileAttachment(String fileAttachment)
-	{
-		return attachments.remove(fileAttachment);
-	}
-	
-	public List getFileAttachments()
-	{
-		return new ArrayList(attachments.keySet());
-	}
-	
-	/*
-	public List getValidFileAttachments()
-	{
-		List list = new ArrayList();
-		for (Iterator iter = attachments.keySet().iterator(); iter.hasNext(); ) {
-			String fileAttachment = (String) iter.next();
-			if (Boolean.TRUE.equals( attachments.get(fileAttachment)) ) {
-				list.add( fileAttachment );
-			}
-		}
-		
-		return list;
-	}
-	*/
+        /* (non-Javadoc)
+         * @see org.springframework.mail.SimpleMailMessage#getText()
+         */
+        @Override
+        public String getText() {
+                if (StringUtils.isNotBlank(textTemplate) && textModel != null && StringUtils.isBlank(super.getText())) {
+                        setText(StrSubstitutor.replace(textTemplate, textModel));
+                }
+
+                if (null != super.getText()) {
+                        return super.getText();
+                }
+
+                // force send empty text message
+                return "";
+        }
+
+        public boolean addFileAttachment(String fileAttachment) {
+                File test = new File(fileAttachment);
+                boolean exist = test.exists();
+
+                // add fileAttachment as key, and check is exist and save it as value of map
+                if (exist) {
+                        attachments.put(fileAttachment, Boolean.valueOf(exist));
+                }
+                return exist;
+        }
+
+        public boolean setFileAttachment(String fileAttachment) {
+                File test = new File(fileAttachment);
+                boolean exist = test.exists();
+
+                // add fileAttachment as key, and check is exist and save it as value of map
+                if (exist) {
+                        attachments.clear();
+                        attachments.put(fileAttachment, Boolean.valueOf(exist));
+                }
+                return exist;
+        }
+
+        public Object removeFileAttachment(String fileAttachment) {
+                return attachments.remove(fileAttachment);
+        }
+
+        public List getFileAttachments() {
+                return new ArrayList(attachments.keySet());
+        }
+        
+        
+        /*
+        public List getValidFileAttachments()
+        {
+        List list = new ArrayList();
+        for (Iterator iter = attachments.keySet().iterator(); iter.hasNext(); ) {
+        String fileAttachment = (String) iter.next();
+        if (Boolean.TRUE.equals( attachments.get(fileAttachment)) ) {
+        list.add( fileAttachment );
+        }
+        }
+        
+        return list;
+        }
+         */
 }
