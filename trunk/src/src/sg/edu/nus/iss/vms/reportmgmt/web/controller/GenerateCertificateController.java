357889e96d94885d93184a1dcf237c0fd2a9d295
a0065963@gmail.com <a0065963@gmail.com@737b461a-4eb2-ba18-f755-00632674a257>
2012-01-02 17:37:54 +0000

    Added logger and amended code.

--- trunk/src/src/sg/edu/nus/iss/vms/reportmgmt/web/controller/GenerateCertificateController.java
+++ trunk/src/src/sg/edu/nus/iss/vms/reportmgmt/web/controller/GenerateCertificateController.java
@@ -36,136 +36,233 @@ public class GenerateCertificateController extends
 	private ProjectManagementService projectManagementService;
 
 	public CodeManagementServices getCodeManagementServices() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("getCodeManagementServices() - start");
+		}
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("getCodeManagementServices() - end");
+		}
 		return codeManagementServices;
 	}
 
 	public void setCodeManagementServices(
 			CodeManagementServices codeManagementServices) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("setCodeManagementServices(CodeManagementServices) - start");
+		}
+
 		this.codeManagementServices = codeManagementServices;
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("setCodeManagementServices(CodeManagementServices) - end");
+		}
 	}
+
 	public VolunteerManagementService getVolunteerManagementService() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("getVolunteerManagementService() - start");
+		}
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("getVolunteerManagementService() - end");
+		}
 		return volunteerManagementService;
 	}
 
 	public void setVolunteerManagementService(
 			VolunteerManagementService volunteerManagementService) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("setVolunteerManagementService(VolunteerManagementService) - start");
+		}
+
 		this.volunteerManagementService = volunteerManagementService;
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("setVolunteerManagementService(VolunteerManagementService) - end");
+		}
 	}
 
 	public ProjectManagementService getProjectManagementService() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("getProjectManagementService() - start");
+		}
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("getProjectManagementService() - end");
+		}
 		return projectManagementService;
 	}
 
 	public void setProjectManagementService(
 			ProjectManagementService projectManagementService) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("setProjectManagementService(ProjectManagementService) - start");
+		}
+
 		this.projectManagementService = projectManagementService;
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("setProjectManagementService(ProjectManagementService) - end");
+		}
 	}
 
 	public CertificateManagement getCertificateManagement() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("getCertificateManagement() - start");
+		}
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("getCertificateManagement() - end");
+		}
 		return certificateManagement;
 	}
 
 	public void setCertificateManagement(
 			CertificateManagement certificateManagement) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("setCertificateManagement(CertificateManagement) - start");
+		}
+
 		this.certificateManagement = certificateManagement;
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("setCertificateManagement(CertificateManagement) - end");
+		}
 	}
 
 	public ReportManagementServices getReportManagementServices() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("getReportManagementServices() - start");
+		}
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("getReportManagementServices() - end");
+		}
 		return this.reportManagementServices;
 	}
 
 	public void setReportManagementServices(
 			ReportManagementServices reportManagementServicesImpl) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("setReportManagementServices(ReportManagementServices) - start");
+		}
+
 		this.reportManagementServices = reportManagementServicesImpl;
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("setReportManagementServices(ReportManagementServices) - end");
+		}
 	}
 
 	public ModelAndView generateCertificate(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
+		if (logger.isDebugEnabled()) {
+			logger.debug("generateCertificate(HttpServletRequest, HttpServletResponse) - start");
+		}
+
 		modelAndView = new ModelAndView("reports/generateCertificate");
 
-		CodeDto stsRequested = codeManagementServices.getCodeDtoByCatVal(
+		CodeDto stsRequested = CodeLookupUtil.getCodeDtoByCatVal(
 				VMSConstants.CERTIFICATE_REQUEST_STATUS,
 				VMSConstants.CERTIFICATE_REQUEST_STATUS_REQUESTED);
-		
-			if(request.getParameter("certRequestId")==null){
-				
-				List list = certificateManagement.getReqCertList(stsRequested.getCdId());
-				List certReqVoList=new ArrayList() ;
-				if(list!=null){
-					for(int i=0;i<list.size();i++){
-            		  CertificateRequestDto obj=(CertificateRequestDto) list.get(i);
-            		  CertificateRequestVo voObj=new CertificateRequestVo();
-            		  voObj.setCertReqId(obj.getCertReqId());
-            		  voObj.setPrjId(obj.getPrjId());
-            		  ProjectDto project=projectManagementService.getProject(obj.getPrjId());
-            		  if(project!=null)
-            			  voObj.setPrjName(project.getNme());
-            		  voObj.setReqTp(obj.getReqTp());
-            		  voObj.setReqTpName(CodeLookupUtil.getCodeValueByCodeId(obj.getReqTp()));
-            		  voObj.setReqBy(obj.getReqBy());
-            		  voObj.setReqDte(obj.getReqDte());
-            		  VolunteerVo volunteer=volunteerManagementService.getVolunteer((obj.getReqBy()));
-            		  if(volunteer!=null)
-            			  voObj.setReqByName(volunteer.getNme());
-            		  certReqVoList.add(voObj);
-					}
-				}
-				System.out.println(" certReqVoList:"+certReqVoList.size());
-				modelAndView.addObject("certReqVoList", certReqVoList);
-			}
-			else{
-				//1. change the status
-				Long certReqId=Long.parseLong(request.getParameter("certRequestId"));
-				CodeDto processedSts = codeManagementServices.getCodeDtoByCatVal(
-						VMSConstants.CERTIFICATE_REQUEST_STATUS,
-						VMSConstants.CERTIFICATE_REQUEST_STATUS_PROCESSED);
-				certificateManagement.updateCertRequestStatus( certReqId, processedSts.getCdId());
-				
-				
-				
-				//2. generate certificate
-				CertificateRequestDto certReqDto=certificateManagement.getCertRequest(certReqId);
-				byte[] bytes = this.reportManagementServices
-				.generateVolunteerCertificate(certReqId, certReqDto.getReqBy(),CodeLookupUtil.getCodeDescriptionByCodeId(certReqDto.getReqTp()));
 
-				if (bytes != null && bytes.length != 0) {
+		if (request.getParameter("certRequestId") == null) {
+
+			List<CertificateRequestDto> list = certificateManagement.getReqCertList(stsRequested.getCdId());
+			List certReqVoList = new ArrayList();
+			
+			if (list != null && list.size() != 0) {
+				for (int i = 0; i < list.size(); i++) {
+					CertificateRequestDto obj = (CertificateRequestDto) list.get(i);
+					CertificateRequestVo voObj = new CertificateRequestVo();
+					voObj.setCertReqId(obj.getCertReqId());
+					voObj.setPrjId(obj.getPrjId());
+					ProjectDto project = projectManagementService.getProject(obj.getPrjId());
+					
+					if (project != null)
+						voObj.setPrjName(project.getNme());
+					
+					voObj.setReqTp(obj.getReqTp());
+					voObj.setReqTpName(CodeLookupUtil.getCodeValueByCodeId(obj.getReqTp()));
+					voObj.setReqBy(obj.getReqBy());
+					voObj.setReqDte(obj.getReqDte());
+					VolunteerVo volunteer = volunteerManagementService.getVolunteer((obj.getReqBy()));
+					
+					if (volunteer != null)
+						voObj.setReqByName(volunteer.getNme());
+					
+					certReqVoList.add(voObj);
+				}
+			}
+			if (logger.isDebugEnabled()) {
+				logger.debug("generateCertificate(HttpServletRequest, HttpServletResponse) - certReqVoList:" + certReqVoList.size());
+			}
+			modelAndView.addObject("certReqVoList", certReqVoList);
+		} else {
+			// 1. change the status
+			Long certReqId = Long.parseLong(request.getParameter("certRequestId"));
+			CodeDto processedSts = CodeLookupUtil.getCodeDtoByCatVal(
+					VMSConstants.CERTIFICATE_REQUEST_STATUS,
+					VMSConstants.CERTIFICATE_REQUEST_STATUS_PROCESSED);
+			certificateManagement.updateCertRequestStatus(certReqId, processedSts.getCdId());
+
+			// 2. generate certificate
+			CertificateRequestDto certReqDto = certificateManagement.getCertRequest(certReqId);
+			byte[] bytes = this.reportManagementServices.generateVolunteerCertificate(certReqId, certReqDto
+							.getReqBy(), CodeLookupUtil
+							.getCodeDescriptionByCodeId(certReqDto.getReqTp()));
+
+			if (bytes != null && bytes.length != 0) {
 				response.setContentType("application/pdf");
-				response.addHeader("Content-Disposition",
-					"attachment;filename= volunteer_certificate.pdf");
+				response.addHeader("Content-Disposition", "attachment;filename= volunteer_certificate.pdf");
 				response.setContentLength(bytes.length);
 				ServletOutputStream outStream = response.getOutputStream();
 				outStream.write(bytes, 0, bytes.length);
 				outStream.flush();
-				outStream.close();	
-				
-				//3. get the remaining request list...
+				outStream.close();
+
+				// 3. get the remaining request list...
 				List list = certificateManagement.getReqCertList(stsRequested.getCdId());
-				List certReqVoList=new ArrayList();
-				
-				if(list!=null){
-					for(int i=0;i<list.size();i++){
-            		  CertificateRequestDto obj=(CertificateRequestDto) list.get(i);
-            		  CertificateRequestVo voObj=new CertificateRequestVo();
-            		  voObj.setCertReqId(obj.getCertReqId());
-            		  voObj.setPrjId(obj.getPrjId());
-            		  ProjectDto project=projectManagementService.getProject(obj.getPrjId());
-            		  if(project!=null)
-            			  voObj.setPrjName(project.getNme());
-            		  voObj.setReqTp(obj.getReqTp());
-            		  voObj.setReqTpName(CodeLookupUtil.getCodeValueByCodeId(obj.getReqTp()));
-            		  voObj.setReqBy(obj.getReqBy());
-            		  voObj.setReqDte(obj.getReqDte());
-            		  VolunteerVo volunteer=volunteerManagementService.getVolunteer((obj.getReqBy()));
-            		  if(volunteer!=null)
-            			  voObj.setReqByName(volunteer.getNme());
-            		  certReqVoList.add(voObj);
+				List certReqVoList = new ArrayList();
+
+				if (list != null && list.size() != 0) {
+					for (int i = 0; i < list.size(); i++) {
+						CertificateRequestDto obj = (CertificateRequestDto) list
+								.get(i);
+						CertificateRequestVo voObj = new CertificateRequestVo();
+						voObj.setCertReqId(obj.getCertReqId());
+						voObj.setPrjId(obj.getPrjId());
+						ProjectDto project = projectManagementService.getProject(obj.getPrjId());
+						
+						if (project != null)
+							voObj.setPrjName(project.getNme());
+						
+						voObj.setReqTp(obj.getReqTp());
+						voObj.setReqTpName(CodeLookupUtil.getCodeValueByCodeId(obj.getReqTp()));
+						voObj.setReqBy(obj.getReqBy());
+						voObj.setReqDte(obj.getReqDte());
+						VolunteerVo volunteer = volunteerManagementService.getVolunteer((obj.getReqBy()));
+						
+						if (volunteer != null)
+							voObj.setReqByName(volunteer.getNme());
+						
+						certReqVoList.add(voObj);
 					}
 				}
 				modelAndView.addObject("certReqVoList", certReqVoList);
-				return modelAndView;
+
+				if (logger.isDebugEnabled()) {
+					logger.debug("generateCertificate(HttpServletRequest, HttpServletResponse) - end");
 				}
+				return modelAndView;
 			}
-			return modelAndView;	
+		}
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("generateCertificate(HttpServletRequest, HttpServletResponse) - end");
+		}
+		return modelAndView;
 	}
 
 }
