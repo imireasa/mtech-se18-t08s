730f2e707bfa31585cfe958208fd85ac5057b6e5
thidakmt <thidakmt@737b461a-4eb2-ba18-f755-00632674a257>
2012-01-02 09:25:56 +0000

    add the project interest vos...

---
+++ trunk/src/src/sg/edu/nus/iss/vms/project/vo/ProjectInterestSearchVo.java
@@ -0,0 +1,29 @@
+package sg.edu.nus.iss.vms.project.vo;
+
+public class ProjectInterestSearchVo {
+	private Long prjId;
+	private String projNme;
+	private String prjIntStatus;
+	
+	public Long getPrjId() {
+		return prjId;
+	}
+	public void setPrjId(Long prjId) {
+		this.prjId = prjId;
+	}
+	public String getProjNme() {
+		return projNme;
+	}
+	public void setProjNme(String projNme) {
+		this.projNme = projNme;
+	}
+	public String getPrjIntStatus() {
+		return prjIntStatus;
+	}
+	public void setPrjIntStatus(String prjIntStatus) {
+		this.prjIntStatus = prjIntStatus;
+	}
+	
+	
+
+}
