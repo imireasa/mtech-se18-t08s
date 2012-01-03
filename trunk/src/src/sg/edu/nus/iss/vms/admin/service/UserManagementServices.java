ee0f6ff7b405b20207482faff21b60c25987ce03
thidakmt <thidakmt@737b461a-4eb2-ba18-f755-00632674a257>
2012-01-03 16:18:46 +0000

    edit for forgetPassword

--- trunk/src/src/sg/edu/nus/iss/vms/admin/service/UserManagementServices.java
+++ trunk/src/src/sg/edu/nus/iss/vms/admin/service/UserManagementServices.java
@@ -2,7 +2,6 @@ package sg.edu.nus.iss.vms.admin.service;
 
 import java.util.List;
 
-import sg.edu.nus.iss.vms.common.dto.CodeDto;
 import sg.edu.nus.iss.vms.common.exception.ApplicationException;
 import sg.edu.nus.iss.vms.security.dto.RoleDto;
 
@@ -10,4 +9,6 @@ public interface UserManagementServices {
 
 	public List<RoleDto> getRoleListByUserLoginId(String userLoginId);
 	public void updatePassword(String email, String currentPassword, String newPassword, String userLoginId) throws ApplicationException;
+	public boolean isLoginIdExists(String loginId);
+	public boolean forgetPassword(String userLoginId) throws ApplicationException;
 }
