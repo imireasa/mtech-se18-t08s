d65bd3d0e2af0959f844d49c747bf5a6391cd07b
thidakmt <thidakmt@737b461a-4eb2-ba18-f755-00632674a257>
2012-01-06 11:13:07 +0000

    update the generic method to generate PDF

--- trunk/src/src/sg/edu/nus/iss/vms/reportmgmt/service/ReportManagementServices.java
+++ trunk/src/src/sg/edu/nus/iss/vms/reportmgmt/service/ReportManagementServices.java
@@ -1,14 +1,13 @@
 package sg.edu.nus.iss.vms.reportmgmt.service;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 import net.sf.jasperreports.engine.JRException;
 
 public interface ReportManagementServices {
 
-	public byte[] generateVolunteerCertificate(Long certRequestId,String reqBy,String requestType) throws JRException,SQLException;
+//	public byte[] generateVolunteerCertificate(Long certRequestId,String reqBy,String requestType) throws JRException,SQLException;
 
-        //Need to get certificate request
-
-
+	public byte[] generatePDFReport(String jrxmlPath,String jasperPath, Map params,String queryString) throws JRException,SQLException;
 }
