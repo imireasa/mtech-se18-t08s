cf45c50b77f8610c7724a08a1b5ebf9320aae492
zwhtet <zwhtet@737b461a-4eb2-ba18-f755-00632674a257>
2012-01-13 07:47:49 +0000

    saveVolunteer to createVolunteer naming standard fix

--- src/src/sg/edu/nus/iss/vms/volunteer/service/VolunteerManagementService.java
+++ src/src/sg/edu/nus/iss/vms/volunteer/service/VolunteerManagementService.java
@@ -4,7 +4,7 @@ import sg.edu.nus.iss.vms.volunteer.vo.VolunteerVo;
 
 public interface VolunteerManagementService {
 
-        public void saveVolunteer(VolunteerVo volunteerVo) throws Exception;
+        public void createVolunteer(VolunteerVo volunteerVo) throws Exception;
 
         boolean isLoginIdExists(String loginId);
 
